c5ed2d8ca8f459a03214e0ed135aa22a52f63f31
bbennett7 <bryn.bennett@gmail.com>
2019-05-22 18:01:01 -0700

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -8,7 +8,6 @@ describe('Fix the Scope', function() {
   describe('upperCaseCustomerName()', function() {
     it('modifies the customerName variable', function() {
       expect(window.customerName).to.equal('bob');
-
       upperCaseCustomerName();
 
       expect(window.customerName).to.equal('BOB');
