047e48307229aa5f737350c778306fc3f411e8cb
joanrig <github email address>
2019-05-23 02:00:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+
+var customerName = 'bob'
+
+function customerName() {
+  return customerName
+}
+
+function upperCaseCustomerName() {
+  customerName =  customerName.toUpperCase()
+  return customerName
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+  return bestCustomer
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'friend of bob'
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'trump'
+}
